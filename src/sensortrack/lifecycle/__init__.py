a25847573470bc0f72e9c859531cb66ac1c888da
Kenneth J. Pronovici <pronovic@ieee.org>
2022-06-12 14:43:21 -0500

    Fix comments

--- src/sensortrack/lifecycle/__init__.py
+++ src/sensortrack/lifecycle/__init__.py
@@ -1,4 +1,1 @@
 __all__ = []  # type: ignore
-
-# TODO: I think what I want to do here is expose only the public interface, keeping everything else private
-#       not sure exactly how I want to do that.
