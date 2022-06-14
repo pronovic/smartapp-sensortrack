0538a8dfb31ecd7adf79d9649dcb71c9d905b4c1
Kenneth J. Pronovici <pronovic@ieee.org>
2022-06-14 01:21:45 -0500

    Continued work on signature verification

--- src/smartapp/signature.py
+++ src/smartapp/signature.py
@@ -5,6 +5,19 @@
 Verify HTTP signatures on SmartApp lifecycle event requests.
 """
 
+# This implements HTTP signature verification for the SmartApp lifecycle events.
+# See: https://developer-preview.smartthings.com/docs/connected-services/hosting/webhook-smartapp/
+#
+# SmartThings uses Joyent's HTTP signature scheme.  Note that only the rsa-sha256 algorithm
+# is supported in this code.  As far as I can tell, this is the only one SmartThings uses.
+# Limiting to the single algorithm greatly simplifies the implementation, plus we can use
+# the test cases that Joylent includes in their specification document in our own unit tests.
+# See: https://github.com/TritonDataCenter/node-http-signature/blob/master/http_signing.md
+#
+# The crypto implementation is PyCryptodome. There are other options, but this appears to
+# be well-maintained, is fairly popular (2000+ GitHub stars), and the documentation is good.
+# See: https://pypi.org/project/pycryptodomex/ and https://www.pycryptodome.org/en/latest/
+
 import re
 import urllib.parse
 from base64 import b64encode
@@ -16,36 +29,38 @@ from attrs import field, frozen
 from Cryptodome.Hash import SHA256
 from Cryptodome.PublicKey import RSA
 from Cryptodome.Signature import pkcs1_15
+from pendulum import now
+from pendulum.datetime import DateTime
+from pendulum.parser import parse
+from requests import ConnectionError as RequestsConnectionError
+from requests import HTTPError, RequestException
+from tenacity import retry
+from tenacity.retry import retry_if_exception_type
+from tenacity.stop import stop_after_attempt
+from tenacity.wait import wait_exponential
 
-from smartapp.interface import SignatureError, SmartAppDefinition, SmartAppDispatcherConfig, SmartAppRequestContext
-
-REQUEST_TARGET = "(request-target)"
-DIGEST = "digest"
-AUTHORIZATION = "authorization"
+from .interface import SignatureError, SmartAppDefinition, SmartAppDispatcherConfig, SmartAppRequestContext
 
 
+# This is implemented as a function so we can cache the result and implement retries outside of SignatureVerifier.
 @lru_cache(maxsize=32)
+@retry(
+    # retry up to 5 times (6 attempts total); wait 0.25 sec before first retry; limit wait between retries to 2 sec
+    stop=stop_after_attempt(5),
+    wait=wait_exponential(multiplier=0.25, max=2),
+    retry=retry_if_exception_type((RequestsConnectionError, HTTPError)),
+)
 def _retrieve_public_key(key_server_url: str, key_id: str) -> str:
     """Retrieve a public key, caching the result."""
     url = "%s/%s" % (key_server_url, key_id)
     response = requests.get(url)
-    if not response.ok:
-        raise SignatureError("Failed to retrieve key: %s" % key_id)
+    response.raise_for_status()
     return response.text
 
 
-def verify_rsa_sha256_signature(public_key: str, message: str, signature: str) -> None:
-    """Verify an RSA-SHA256 signature for a message."""
-    try:
-        key = RSA.import_key(public_key)
-        sha256 = SHA256.new(message.encode())
-        pkcs1_15.new(key).verify(sha256, signature.encode())
-    except (ValueError, TypeError) as e:
-        raise SignatureError("Signature is not valid") from e
-
-
+# noinspection PyUnresolvedReferences
 @frozen(kw_only=True, repr=False)
-class SignatureContext:
+class SignatureVerifier:
 
     context: SmartAppRequestContext
     config: SmartAppDispatcherConfig
@@ -54,6 +69,7 @@ class SignatureContext:
     method: str = field()
     path: str = field()
     request_target: str = field()
+    date: DateTime = field()
     authorization: str = field()
     signing_attributes: Mapping[str, str] = field()
     key_id: str = field()
@@ -64,36 +80,34 @@ class SignatureContext:
     signing_string: str = field()
     signature: str = field()
 
-    # noinspection PyUnresolvedReferences
     @body.default
     def _default_body(self) -> str:
         return self.context.body
 
-    # noinspection PyUnresolvedReferences
     @method.default
     def _default_method(self) -> str:
         return "POST"  # this is the only method ever used by the SmartApp interface
 
-    # noinspection PyUnresolvedReferences
     @path.default
     def _default_path(self) -> str:
-        # We need to use the path from the configured endpoint, which might
-        # be different than the server served, due to forwarding.
+        # The path from the configured endpoint might be different than what the server served, due to forwarding
         return urllib.parse.urlparse(self.definition.target_url).path
 
-    # noinspection PyUnresolvedReferences
     @request_target.default
     def _default_request_target(self) -> str:
         return "%s %s" % (self.method.lower(), self.path)
 
-    # noinspection PyUnresolvedReferences
     @authorization.default
     def _default_authorization(self) -> str:
-        return self.header(AUTHORIZATION)
+        return self.header("authorization")
 
-    # noinspection PyUnresolvedReferences
+    @date.default
+    def _default_date(self) -> DateTime:
+        return parse(self.header("date"))  # type: ignore
+
     @signing_attributes.default
     def _default_signing_attributes(self) -> Mapping[str, str]:
+        # We're parsing a string like: 'Signature keyId="key",algorithm="rsa-sha256",headers="date",signature="xxx"'
         def attribute(name: str) -> str:
             if not self.authorization.startswith("Signature "):
                 raise SignatureError("Authorization header is not a signature")
@@ -105,17 +119,14 @@ class SignatureContext:
 
         return {name: attribute(name) for name in ["keyId", "headers", "algorithm", "signature"]}
 
-    # noinspection PyUnresolvedReferences
     @key_id.default
     def _default_key_id(self) -> str:
         return self.signing_attributes["keyId"]
 
-    # noinspection PyUnresolvedReferences
     @key_url.default
     def _default_key_server_url(self) -> str:
         return self.config.key_server_url
 
-    # noinspection PyUnresolvedReferences
     @algorithm.default
     def _default_algorithm(self) -> str:
         algorithm = self.signing_attributes["algorithm"]
@@ -123,12 +134,10 @@ class SignatureContext:
             raise SignatureError("Algorithm not supported: %s" % algorithm)
         return algorithm
 
-    # noinspection PyUnresolvedReferences
     @signature.default
     def _default_signature(self) -> str:
         return self.signing_attributes["signature"]
 
-    # noinspection PyUnresolvedReferences
     @digest.default
     def _default_digest(self) -> str:
         data = self.body.encode()
@@ -136,20 +145,19 @@ class SignatureContext:
         base64 = b64encode(digest).decode("ascii")
         return "SHA-256=%s" % base64
 
-    # noinspection PyUnresolvedReferences
     @signing_headers.default
     def _default_signing_headers(self) -> List[str]:
+        # We're parsing a string like "(request-target) date content-type digest" into a list
         return self.signing_attributes["headers"].strip().split(" ")
 
-    # noinspection PyUnresolvedReferences
     @signing_string.default
     def _signing_string(self) -> str:
         components = []
         for name in self.signing_headers:
-            if name == REQUEST_TARGET:
-                components.append("%s: %s" % (REQUEST_TARGET, self.request_target))
-            elif name == DIGEST:
-                components.append("%s: %s" % (DIGEST, self.digest))
+            if name == "(request-target)":
+                components.append("(request-target): %s" % self.request_target)
+            elif name == "digest":
+                components.append("digest: %s" % self.digest)
             else:
                 components.append("%s: %s" % (name, self.header(name)))
         return "\n".join(components).rstrip("\n")
@@ -163,10 +171,37 @@ class SignatureContext:
             raise SignatureError("Header not found: %s" % name)
         return value
 
+    def retrieve_public_key(self) -> str:
+        """Retrieve the configured public key."""
+        try:
+            return _retrieve_public_key(self.key_url, self.key_id)  # will retry automatically
+        except RequestException as e:
+            raise SignatureError("Failed to retrieve key [%s]" % self.key_id) from e
 
-# pylint: disable=unused-argument:
+    def verify_date(self) -> None:
+        """Verify the date, ensuring that it is current per skew configuration."""
+        if self.config.clock_skew_sec is not None:
+            skew = now() - self.date
+            if abs(skew.seconds) > self.config.clock_skew_sec:
+                raise SignatureError("Request date is not current, skew of %d seconds" % skew.seconds)
+
+    def verify_signature(self) -> None:
+        """Verify the RSA-SHA256 signature of the signing string."""
+        # See: https://www.pycryptodome.org/en/latest/src/signature/pkcs1_v1_5.html
+        try:
+            key = RSA.import_key(self.retrieve_public_key())
+            sha256 = SHA256.new(self.signing_string.encode())
+            pkcs1_15.new(key).verify(sha256, self.signature.encode())
+        except (ValueError, TypeError) as e:
+            raise SignatureError("Signature is not valid") from e
+
+    def verify(self) -> None:
+        """Verify the signature."""
+        self.verify_date()
+        self.verify_signature()
+
+
+# This is implemented as a function to make mocking and stubbing easier
 def check_signature(context: SmartAppRequestContext, config: SmartAppDispatcherConfig, definition: SmartAppDefinition) -> None:
     """Verify the signature on a SmartApp request, raising SignatureError if invalid."""
-    signature = SignatureContext(context=context, config=config, definition=definition)
-    public_key = _retrieve_public_key(signature.key_url, signature.key_id)
-    verify_rsa_sha256_signature(public_key, signature.signing_string, signature.signature)
+    SignatureVerifier(context=context, config=config, definition=definition).verify()
