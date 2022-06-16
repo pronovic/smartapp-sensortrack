65d5d28a42c584bc3e3b41bdc6c3ca2f914c2a41
Kenneth J. Pronovici <pronovic@ieee.org>
2022-06-15 21:26:50 -0500

    Add some comments in event handler

--- src/sensortrack/handler.py
+++ src/sensortrack/handler.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # vim: set ft=python ts=4 sw=4 expandtab:
+# pylint: disable=unnecessary-pass:
 
 """
 SmartApp event handler.
@@ -23,22 +24,29 @@ class EventHandler(SmartAppEventHandler):
     """SmartApp event handler."""
 
     def handle_confirmation(self, correlation_id: Optional[str], request: ConfirmationRequest) -> None:
-        pass
+        """Handle a CONFIRMATION lifecycle request"""
+        pass  # no action needed for this event, the standard dispatcher does everything that's needed
 
     def handle_configuration(self, correlation_id: Optional[str], request: ConfigurationRequest) -> None:
-        pass
-
-    def handle_install(self, correlation_id: Optional[str], request: InstallRequest) -> None:
-        pass
-
-    def handle_update(self, correlation_id: Optional[str], request: UpdateRequest) -> None:
-        pass
+        """Handle a CONFIGURATION lifecycle request."""
+        pass  # no action needed for this event, the standard dispatcher does everything that's needed
 
     def handle_uninstall(self, correlation_id: Optional[str], request: UninstallRequest) -> None:
-        pass
+        """Handle an UNINSTALL lifecycle request."""
+        pass  # no action needed for this event, subscriptions and schedules have already been deleted
 
     def handle_oauth_callback(self, correlation_id: Optional[str], request: OauthCallbackRequest) -> None:
-        pass
+        """Handle an OAUTH_CALLBACK lifecycle request."""
+        pass  # no action needed for this event, we don't use any special oauth integration
+
+    def handle_install(self, correlation_id: Optional[str], request: InstallRequest) -> None:
+        """Handle an INSTALL lifecycle request."""
+        pass  # TODO: subscribe to state change events for configured devices
+
+    def handle_update(self, correlation_id: Optional[str], request: UpdateRequest) -> None:
+        """Handle an UPDATE lifecycle request."""
+        pass  # TODO: delete and replace existing subscriptions for configured devices
 
     def handle_event(self, correlation_id: Optional[str], request: EventRequest) -> None:
-        pass
+        """Handle an EVENT lifecycle request."""
+        pass  # TODO: handle events where we get data when sensors change state (not sure exactly how this works)
