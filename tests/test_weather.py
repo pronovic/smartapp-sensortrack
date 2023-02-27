57195b27622dca1fb085221bc165d93cfa44febc
Kenneth J. Pronovici <pronovic@users.noreply.github.com>
2023-02-26 18:25:38 -0600

    Rewrite tests to use the responses framework (#18)

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -1,60 +1,51 @@
 # -*- coding: utf-8 -*-
 # vim: set ft=python ts=4 sw=4 expandtab:
-import json
 import os
-from unittest.mock import MagicMock, call, patch
+from unittest.mock import MagicMock, patch
 
 import pytest
-from requests import HTTPError
+import responses
+from responses import matchers
+from responses.registries import OrderedRegistry
 
 from sensortrack.weather import retrieve_current_conditions
 from tests.testutil import load_file
 
 FIXTURE_DIR = os.path.join(os.path.dirname(__file__), "fixtures")
+TIMEOUT_MATCHER = matchers.request_kwargs_matcher({"timeout": 5.0})
 
 
 class TestPublicFunctions:
-    @patch("sensortrack.weather.raise_for_status")
-    @patch("sensortrack.weather.requests")
     @patch("sensortrack.weather.config")
-    def test_retrieve_current_conditions(self, config, requests, raise_for_status):
+    def test_retrieve_current_conditions(self, config):
         config.return_value = MagicMock(weather=MagicMock(base_url="https://base"))
+        with responses.RequestsMock(registry=OrderedRegistry) as r:
+            r.get(
+                url="https://base/points/12.3,45.6/stations",
+                status=500,
+                match=[TIMEOUT_MATCHER],
+            )
+            r.get(
+                url="https://base/points/12.3,45.6/stations",
+                status=200,
+                body=load_file(os.path.join(FIXTURE_DIR, "weather", "stations.json")),
+                match=[TIMEOUT_MATCHER],
+            )
+            r.get(
+                url="https://api.weather.gov/stations/KALO/observations/latest",  # first station from JSON, which is closest
+                status=500,
+                match=[TIMEOUT_MATCHER],
+            )
+            r.get(
+                url="https://api.weather.gov/stations/KALO/observations/latest",  # first station from JSON, which is closest
+                status=200,
+                body=load_file(os.path.join(FIXTURE_DIR, "weather", "observations", "valid.json")),
+                match=[TIMEOUT_MATCHER],
+            )
+            # expected temperature taken from Google, to sanity-check library
+            assert retrieve_current_conditions(latitude=12.3, longitude=45.6) == (84.92, 41.59)
+            assert len(r.calls) == 4  # one retry and one success for each endpoint
 
-        exception = HTTPError("hello")
-
-        stations = load_file(os.path.join(FIXTURE_DIR, "weather", "stations.json"))
-        stations_url = "https://base/points/12.3,45.6/stations"
-        stations_response = MagicMock(json=MagicMock(return_value=json.loads(stations)))
-
-        observations = load_file(os.path.join(FIXTURE_DIR, "weather", "observations", "valid.json"))
-        observations_url = "https://api.weather.gov/stations/KALO/observations/latest"  # first station from JSON, which is closest
-        observations_response = MagicMock(json=MagicMock(return_value=json.loads(observations)))
-
-        # each exception is thrown, and that is handled by the retry annotation
-        requests.get = MagicMock(
-            side_effect=[exception, stations_response, exception, observations_response, observations_response]
-        )
-
-        # expected temperature taken from Google, to sanity-check library
-        assert retrieve_current_conditions(latitude=12.3, longitude=45.6) == (84.92, 41.59)
-
-        requests.get.assert_has_calls(
-            [
-                call(url=stations_url, timeout=5.0),
-                call(url=stations_url, timeout=5.0),
-                call(url=observations_url, timeout=5.0),
-                call(url=observations_url, timeout=5.0),
-            ]
-        )
-        raise_for_status.assert_has_calls(
-            [
-                call(stations_response),
-                call(observations_response),
-            ]
-        )
-
-    @patch("sensortrack.weather.raise_for_status")
-    @patch("sensortrack.weather.requests")
     @patch("sensortrack.weather.config")
     @pytest.mark.parametrize(
         "input_file,expected",
@@ -64,31 +55,20 @@ class TestPublicFunctions:
             ("null.json", (None, None)),
         ],
     )
-    def test_retrieve_current_conditions_bad_data(self, config, requests, raise_for_status, input_file, expected):
+    def test_retrieve_current_conditions_bad_data(self, config, input_file, expected):
         config.return_value = MagicMock(weather=MagicMock(base_url="https://base"))
-
-        stations = load_file(os.path.join(FIXTURE_DIR, "weather", "stations.json"))
-        stations_url = "https://base/points/12.3,45.6/stations"
-        stations_response = MagicMock(json=MagicMock(return_value=json.loads(stations)))
-
-        observations = load_file(os.path.join(FIXTURE_DIR, "weather", "observations", input_file))
-        observations_url = "https://api.weather.gov/stations/KALO/observations/latest"  # first station from JSON, which is closest
-        observations_response = MagicMock(json=MagicMock(return_value=json.loads(observations)))
-
-        requests.get = MagicMock(side_effect=[stations_response, observations_response])
-
-        # expected temperature taken from Google, to sanity-check library
-        assert retrieve_current_conditions(latitude=12.3, longitude=45.6) == expected
-
-        requests.get.assert_has_calls(
-            [
-                call(url=stations_url, timeout=5.0),
-                call(url=observations_url, timeout=5.0),
-            ]
-        )
-        raise_for_status.assert_has_calls(
-            [
-                call(stations_response),
-                call(observations_response),
-            ]
-        )
+        with responses.RequestsMock(registry=OrderedRegistry) as r:
+            r.get(
+                url="https://base/points/12.3,45.6/stations",
+                status=200,
+                body=load_file(os.path.join(FIXTURE_DIR, "weather", "stations.json")),
+                match=[TIMEOUT_MATCHER],
+            )
+            r.get(
+                url="https://api.weather.gov/stations/KALO/observations/latest",  # first station from JSON, which is closest
+                status=200,
+                body=load_file(os.path.join(FIXTURE_DIR, "weather", "observations", input_file)),
+                match=[TIMEOUT_MATCHER],
+            )
+            # expected temperature taken from Google, to sanity-check library
+            assert retrieve_current_conditions(latitude=12.3, longitude=45.6) == expected
